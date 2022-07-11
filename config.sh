cd916bf9385ead8bb2cdb8b8704587d619fd7c0d
allworkdone <raj635321@gmail.com>
2022-07-11 10:16:06 +0530

    bitra: initial build for fox_12.1

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/allworkdone/ofox_device_realme_bitra -b fox_12.1"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="bitra"
+export OEM="realme"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+#export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
